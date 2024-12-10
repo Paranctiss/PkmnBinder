17dd5ee22bf12039c8258c6440082debbe0d96ea
Paranctiss <fli1@live.fr>
2024-12-10 11:12:25 +0100

    first routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/auth.component').then(m => m.AuthComponent)
+  },
+  {
+    path: 'catalog',
+    loadComponent: () => import('./catalog/catalog.component').then(m => m.CatalogComponent)
+  },
+  {
+    path: 'collection',
+    loadComponent: () => import('./collection/collection.component').then(m => m.CollectionComponent)
+  },
+  { path: '**', redirectTo: '/auth' } // Gestion des routes inexistantes
+];
